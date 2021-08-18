c225c96a7e0a921f0c8d8ec36e914c67865ddbd3
Matt Bush <59295261+matthewbush55@users.noreply.github.com>
2021-08-18 15:34:51 -0600

    Created initial file structure

--- index.js
+++ index.js
@@ -1,16 +1,76 @@
 // TODO: Include packages needed for this application
 const fs = require("fs");
 const inquirer = require("inquirer");
-// const generateMarkdown = require("generateMarkdown");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    message: "What is your project title?",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "What should go in the table of contents?",
+    name: "contents",
+  },
+  {
+    type: "input",
+    message: "Provide a brief description of your project",
+    name: "description",
+  },
+  {
+    type: "input",
+    message: "What command should be run for project dependencies?",
+    name: "dependencies",
+    default: "npm i",
+  },
+  {
+    type: "input",
+    message: "Provide usage information for your project",
+    name: "usage",
+  },
+  {
+    type: "input",
+    message: "Describe contribution guidelines for your project",
+    name: "contribution",
+  },
+  {
+    type: "input",
+    message: "What command should be run for tests?",
+    name: "tests",
+    default: "npm run tests",
+  },
+  {
+    type: "list",
+    message: "What type of license is your project covered under?",
+    name: "license",
+    choices: ["MIT", "lic", "lic2"],
+  },
+  {
+    type: "input",
+    message: "What is your GitHub username?",
+    name: "github",
+  },
+  {
+    type: "input",
+    message: "What is your email address?",
+    name: "email",
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
-
+function init() {
+  // present the user with questions
+  inquirer.prompt(questions).then((data) => {
+    fs.writeFile("README.md", JSON.stringify(data), (error) => {
+      error ? console.log(error) : console.log("File created!");
+    });
+  });
+}
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,49 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license != "None") {
+    return `## License
+    This project is licensed with ${license}.`;
+  }
+  return "";
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license != "None") {
+    return `## License Link`;
+    // return the actual link of the license
+  }
+  return "";
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
+function renderLicenseSection(license) {
+  if (license != "None") {
+    return `## License Section`;
+    // return the actual link of the license
+  }
+  return "";
+}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
+  ## Table of Contents
+  ${data.table}
+
+  ## Description
+  ${data.description}
+
+  ## Dependencies
+  ${data.dependencies}
+
+  ## Usage
+  ${data.usage}
+
+  ##
 
 `;
 }
