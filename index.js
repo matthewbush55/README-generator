4ed0a401456d443959f988c57e3824978dd5a6bf
Matt Bush <59295261+matthewbush55@users.noreply.github.com>
2021-08-18 14:49:26 -0600

    Initial commit

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // TODO: Include packages needed for this application
 const fs = require("fs");
 const inquirer = require("inquirer");
-const generateMarkdown = require("generateMarkdown");
+// const generateMarkdown = require("generateMarkdown");
+
 // TODO: Create an array of questions for user input
 const questions = [];
 
